c10dda676f1601bc1daf5629956daeec1848e7cf
vesvirtechnology <vesvirapp@gmail.com>
2025-06-02 22:49:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const express = require('express');
-const { VoiceResponse } = require('twilio').twiml;
+import express from 'express';
+import { twiml } from 'twilio';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 3000;
 
-app.post('/', (req, res) => {
-  const twiml = new VoiceResponse();
-  twiml.say('Hola, esta es una prueba del agente de voz.');
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/', async (req, res) => {
+  const VoiceResponse = twiml.VoiceResponse;
+  const response = new VoiceResponse();
+
+  response.connect().stream({
+    url: 'wss://api.retellai.com/connect',
+    parameter: [
+      { name: 'apiKey', value: process.env.RETELL_API_KEY },
+      { name: 'agentId', value: process.env.RETELL_AGENT_ID }
+    ]
+  });
+
   res.type('text/xml');
-  res.send(twiml.toString());
+  res.send(response.toString());
 });
 
 app.listen(port, () => {
-  console.log(`Servidor corriendo en puerto ${port}`);
+  console.log(`Server is live on port ${port}`);
 });
 
